d9571b983e6ee49129251b815119beef9399bb12
neoscript <neoscript@gmail.com>
2018-07-31 09:30:45 +0800

    移除无关工程

--- ready-player-one/src/main/groovy/com/matrix/rpo/domain/game/ProgressScene.groovy
+++
@@ -1,9 +0,0 @@
-package com.matrix.rpo.domain.game
-
-import grails.gorm.annotation.Entity
-
-@Entity
-class ProgressScene {
-    String id
-    static belongsTo = [progress: Progress, scene: Scene]
-}
