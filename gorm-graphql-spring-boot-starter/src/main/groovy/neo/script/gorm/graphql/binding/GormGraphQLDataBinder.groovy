0b37101ad0615dc5dca0046eb54a38c4d5f8ceb9
neoscript <neoscript@gmail.com>
2018-09-15 11:15:43 +0800

    简单完善

--- gorm-graphql-spring-boot-starter/src/main/groovy/neo/script/gorm/graphql/binding/GormGraphQLDataBinder.groovy
+++ gorm-graphql-spring-boot-starter/src/main/groovy/neo/script/gorm/graphql/binding/GormGraphQLDataBinder.groovy
@@ -1,24 +1,14 @@
 package neo.script.gorm.graphql.binding
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
+import neo.script.util.JsonUtil
 import org.grails.datastore.gorm.GormEntity
 import org.grails.gorm.graphql.binding.GraphQLDataBinder
 
 class GormGraphQLDataBinder implements GraphQLDataBinder {
 
-    private static ObjectMapper objectMapper = createObjectMapper();
-
-    public static ObjectMapper createObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        return objectMapper;
-    }
-
     @Override
     void bind(Object object, Map data) {
-        String jsonData = objectMapper.writeValueAsString(data);
-        def newObject = objectMapper.readValue(jsonData, object.class)
+        def newObject = JsonUtil.mapToBean(data, object.class)
         //1、遍历data，对其中属性进行赋值
         //2、如果属性为GormEntity，通过id查询实体
         //3、遍历赋值，如果直接复制整个newObject，会带入null值，因为data不一定是全量属性
--- gorm-graphql-spring-boot-starter/src/main/groovy/neo/script/gorm/graphql/helper/LazyMapping.groovy
+++ gorm-graphql-spring-boot-starter/src/main/groovy/neo/script/gorm/graphql/helper/LazyMapping.groovy
@@ -5,6 +5,9 @@ import org.grails.gorm.graphql.entity.dsl.GraphQLMapping
 import org.grails.gorm.graphql.entity.dsl.LazyGraphQLMapping
 
 /**
+ * 原先设计，需要GraphQLMapping.lazy的，可以通过 static graphql = new LazyMapping(UserGraphqlMapping.class),这样mapping可以写到单独的类中
+ * 之后加了GraphQLMappingFlag注解，通过spring管理，能得到domain.gormPersistentEntity，所以暂时不再需要这个类
+ *
  *  Because we are accessing the persistent entity,
  *  the GORM mapping context must be created before this code is evaluated
  */
