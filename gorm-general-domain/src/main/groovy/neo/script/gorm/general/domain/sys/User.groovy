6392e8130bd8554508add0c0e38cbdf866f9dcb1
neoscript <neoscript@gmail.com>
2019-07-26 19:52:24 +0800

    User删除lastUser

--- gorm-general-domain/src/main/groovy/neo/script/gorm/general/domain/sys/User.groovy
+++ gorm-general-domain/src/main/groovy/neo/script/gorm/general/domain/sys/User.groovy
@@ -17,37 +17,37 @@ import neo.script.util.EncoderUtil
  * sys包下都是管理后台相关domain
  */
 class User {
-    static final String DEFAULT_PASSWORD = EncoderUtil.sha256('anonymous');
-    static final User ADMIN = (new User(account: 'admin', name: '系统管理员', dept: Department.HEAD_OFFICE,
-            editable: false, password: DEFAULT_PASSWORD))
-    static final User ANONYMOUS = (new User(account: 'anonymous', name: '匿名帐号', dept: Department.HEAD_OFFICE,
-            editable: false, password: DEFAULT_PASSWORD, lastUser: ADMIN))
-    static final User TEST_USER = (new User(account: 'test.user', name: '测试用户', dept: Department.HEAD_OFFICE,
-            editable: true, password: DEFAULT_PASSWORD, lastUser: ADMIN))
-
     String id
     String account
-    String password = EncoderUtil.sha256('change-it')
+    String password = EncoderUtil.sha256('anonymous')
     String name
     Boolean editable = true
     Boolean enabled = true
 
     Department dept
 
-    User lastUser;
+    //User lastUser;
     Date lastUpdated;
     Date dateCreated;
 
     static mapping = {
         dept fetch: 'join', lazy: false
-        lastUser fetch: 'join', lazy: false
+        //lastUser fetch: 'join', lazy: false
     }
     static constraints = {
         account unique: true
         password maxSize: 80
-        lastUser nullable: true
+        //lastUser nullable: true
     }
 
-    static final initList = [ADMIN, ANONYMOUS, TEST_USER]
     static graphql
+
+    static final User ADMIN = (new User(account: 'admin', name: '系统管理员', dept: Department.HEAD_OFFICE,
+            editable: false))
+    static final User ANONYMOUS = (new User(account: 'anonymous', name: '匿名帐号', dept: Department.HEAD_OFFICE,
+            editable: false))
+    static final User TEST_USER = (new User(account: 'test.user', name: '测试用户', dept: Department.HEAD_OFFICE,
+            editable: true))
+
+    static final initList = [ADMIN, ANONYMOUS, TEST_USER]
 }
